c08a111bacc6c0595d46be194458094a29670a0a
Aaron Bergmann <sorcery.ramrod.9d@icloud.com>
2025-10-17 19:47:46 +0000

    Add 03_reverse_num.c

---
+++ challenges/03_reverse_num.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+/*
+3. Reverse a Number
+   - Input: An integer (e.g., 1234)
+   - Output: Digits in reverse (e.g., 4321)
+*/
+int reverseNumber(int t_rev_int) {
+    // convert to string, reverse string
+    // convert to int, return int
+}
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
